43feddb4f836a2699893e6ab6762edbcad27f14e
Tsuzu <cs3238-tsuzu@users.noreply.github.com>
2021-02-18 13:50:35 +0900

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-utils/astutil
+
+go 1.16
---
+++ tag.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2021 go-utils
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package astutil
+
+import (
+	"go/ast"
+	"reflect"
+)
+
+// GetStructField gets the field name or matching tagged name
+func GetStructField(field *ast.Field, tag string) string {
+	if field.Tag != nil {
+		key, found := reflect.StructTag(field.Tag.Value[1 : len(field.Tag.Value)-1]).Lookup(tag)
+
+		if found {
+			return key
+		}
+	}
+
+	return field.Names[0].Name
+}
+
+// FindStructField finds the field for struct matching the name
+func FindStructField(strct *ast.StructType, tag, name string) string {
+	for _, field := range strct.Fields.List {
+		key := GetStructField(field, tag)
+
+		if key == name {
+			return field.Names[0].Name
+		}
+	}
+
+	return ""
+}
